b441be9db65ca116257767f83d014015abfe3b11
Michael Mathu <mathumichael17@gmail.com>
2022-06-22 23:44:05 +0300

    added layout

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Barbershop_Booking_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Barbershop_Booking_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Home");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        public ViewResult Services()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Barbershop_Booking_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
